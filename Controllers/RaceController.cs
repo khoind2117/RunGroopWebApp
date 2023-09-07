2991ed9d2f4db0459abc9534b64224b8c9d3e889
khoind2117 <khoind2117@gmail.com>
2023-09-07 14:12:58 +0700

    Add project files.

---
+++ Controllers/RaceController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Models;
+using RunGroopWebApp.Repository;
+using RunGroopWebApp.Services;
+using RunGroopWebApp.ViewModels;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class RaceController : Controller
+    {
+        private IRaceRepository _raceRepository;
+        private IPhotoService _photoService;
+
+        public RaceController(IRaceRepository raceRepository, IPhotoService photoService)
+        {
+            _raceRepository = raceRepository;
+            _photoService = photoService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Race> races = await _raceRepository.GetAll();
+            return View(races);
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            Race race = await _raceRepository.GetIdByAsync(id);
+            return View(race);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateRaceViewModel raceVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _photoService.AddPhotoASync(raceVM.Image);
+
+                var race = new Race
+                {
+                    Title = raceVM.Title,
+                    Description = raceVM.Description,
+                    Image = result.Url.ToString(),
+                    Address = new Address
+                    {
+                        Street = raceVM.Address.Street,
+                        City = raceVM.Address.City,
+                        State = raceVM.Address.State
+                    }
+                };
+                _raceRepository.Add(race);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Photo upload failed");
+            }
+
+            return View(raceVM);
+        }
+    }
+}
---
+++ Interfaces/IPhotoService.cs
@@ -0,0 +1,10 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace RunGroopWebApp.Interfaces
+{
+    public interface IPhotoService
+    {
+        Task<ImageUploadResult> AddPhotoASync(IFormFile file);
+        Task<DeletionResult> DeletePhotoASync(string publicId);
+    }
+}
